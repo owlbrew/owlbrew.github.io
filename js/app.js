3997b646d1691cdeb712c93ab199c383e826e352
niklas <nyholmniklas@gmail.com>
2016-01-26 23:10:55 +0200

    Formatted app.js indentation

--- js/app.js
+++ js/app.js
@@ -1,94 +1,94 @@
-    /** McFly */
+/** McFly */
 
-    var Flux = new McFly();
+var Flux = new McFly();
 
-    /** Store */
-    _cards = [];
+/** Store */
+_cards = [];
 
-    function updateCards() {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function () {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                _cards = JSON.parse(xmlHttp.responseText);
-            }
-        };
-        xmlHttp.open("GET", "https://api.deckbrew.com/mtg/cards", false); // false for synchronous request
-        xmlHttp.send(null);
+function updateCards() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            _cards = JSON.parse(xmlHttp.responseText);
+        }
+    };
+    xmlHttp.open("GET", "https://api.deckbrew.com/mtg/cards", false); // false for synchronous request
+    xmlHttp.send(null);
+}
+
+var CardStore = Flux.createStore({
+    getCards: function () {
+        return _cards;
     }
+}, function (payload) {
+    if (payload.actionType === "UPDATE_CARDS") {
+        updateCards();
+        CardStore.emitChange();
+    }
+});
 
-    var CardStore = Flux.createStore({
-        getCards: function () {
-            return _cards;
-        }
-    }, function (payload) {
-        if (payload.actionType === "UPDATE_CARDS") {
-            updateCards();
-            CardStore.emitChange();
-        }
-    });
+/** Actions */
 
-    /** Actions */
-
-    var CardsActions = Flux.createActions({
-        updateCards: function () {
-            return {
-                actionType: "UPDATE_CARDS"
-            }
-        }
-    });
-
-    function getCards() {
+var CardsActions = Flux.createActions({
+    updateCards: function () {
         return {
-            cards: CardStore.getCards()
+            actionType: "UPDATE_CARDS"
         }
     }
+});
 
-    /** Controller View */
+function getCards() {
+    return {
+        cards: CardStore.getCards()
+    }
+}
 
-    var CardController = React.createClass({
-        mixins: [CardStore.mixin],
-        getInitialState: function () {
-            return getCards();
-        },
-        storeDidChange: function () {
-            this.setState(getCards());
-        },
-        render: function () {
-            return <Cards cards={this.state.cards}/>;
-        }
-    });
+/** Controller View */
 
-    /** Component */
+var CardController = React.createClass({
+    mixins: [CardStore.mixin],
+    getInitialState: function () {
+        return getCards();
+    },
+    storeDidChange: function () {
+        this.setState(getCards());
+    },
+    render: function () {
+        return <Cards cards={this.state.cards}/>;
+    }
+});
 
-    var Card = React.createClass({
-        render: function () {
-            return (
-                    <div>
-                        <h1>{this.props.data.name}</h1>
-                        <img src={this.props.data.editions[0].image_url}/>
-                    </div>
-            )
-        }
-    });
+/** Component */
 
-    var Cards = React.createClass({
-        getCards: function () {
-            CardsActions.updateCards();
-        },
-        render: function () {
-            return (
-                    <div className="mtg_app">
-                        <ul className="cards">
-                            { this.props.cards.map(function (card) {
-                                return <li key={card.id}>
-                                    <Card data={card}/>
-                                </li>
-                            })}
-                        </ul>
-                        <button onClick={this.getCards}>UpdateCards</button>
-                    </div>
-            )
-        }
-    });
+var Card = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>{this.props.data.name}</h1>
+                <img src={this.props.data.editions[0].image_url}/>
+            </div>
+        )
+    }
+});
 
-    React.render(<CardController />, document.body);
+var Cards = React.createClass({
+    getCards: function () {
+        CardsActions.updateCards();
+    },
+    render: function () {
+        return (
+            <div className="mtg_app">
+                <ul className="cards">
+                    { this.props.cards.map(function (card) {
+                        return <li key={card.id}>
+                            <Card data={card}/>
+                        </li>
+                    })}
+                </ul>
+                <button onClick={this.getCards}>UpdateCards</button>
+            </div>
+        )
+    }
+});
+
+React.render(<CardController />, document.body);
