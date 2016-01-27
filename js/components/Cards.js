a9bec03e1cf6f8a978297947ac49927cc67c68a4
niklas <nyholmniklas@gmail.com>
2016-01-27 20:46:35 +0200

    Some more card and cards css magic

--- js/components/Cards.js
+++ js/components/Cards.js
@@ -1,7 +1,7 @@
 var Cards = React.createClass({
     render: function () {
         return (
-            <div className="mtg_app">
+            <div>
                 <ul className="cards">
                     { this.props.cards.map(function (card) {
                         return <li key={card.id}>
