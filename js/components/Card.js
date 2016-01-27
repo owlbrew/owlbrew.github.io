88b710b79d02ae39e3784175d893208b8dd7935e
niklas <nyholmniklas@gmail.com>
2016-01-27 20:38:20 +0200

    Moved some stuff around and created Card.css

--- js/components/Card.js
+++ js/components/Card.js
@@ -1,7 +1,7 @@
 var Card = React.createClass({
     render: function () {
         return (
-            <div>
+            <div className="card">
                 <h1>{this.props.data.name}</h1>
                 <img src={this.props.data.editions[0].image_url}/>
             </div>
